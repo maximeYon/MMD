9dd82c862994661d1b0300806fcdb9696f0e0839
Markus Nilsson <nilzzon@gmail.com>
2016-02-19 11:23:29 +0100

    preliminary implementation of fexi 2011 lasic

---
+++ models/fexi11/fexi11_1d_data2fit.m
@@ -0,0 +1,71 @@
+function m = fexi11_1d_data2fit(signal, xps, opt, ind)
+% function m = fexi11_1d_data2fit(signal, xps, opt, ind)
+%
+% Yields a 1xN vector 'm' with the fit parameters
+% m(1)   - ADC0
+% m(2)   - sigma
+% m(3)   - AXR
+% m(4:end) - vector of S0, length(m) = 3+max(xps.mde_t12_ind)
+
+if (nargin < 4), ind = ~isnan(signal); end
+
+signal = double(signal);
+
+S0 = signal(xps.mde_b2_ind==1);
+
+t_guess    = [1.000  0.5 4.00 ones(size(S0'))];
+t_lb       = [0.001  0.0 0.00 zeros(size(S0'))];
+t_ub       = [4.000  1.0 20.0 2*ones(size(S0'))];
+
+unit_to_SI = [1e-9   1.0 1.00  S0'];
+
+    function s = fun(t,varargin)
+        m = t .* unit_to_SI;
+        s = fexi11_1d_fit2data(m, xps);
+        s = s(ind);
+    end
+
+t = lsqcurvefit(@fun, t_guess, [], signal(ind), ...
+    t_lb, t_ub, opt.fexi11.lsq_opts);
+
+m = t .* unit_to_SI;
+
+
+
+% exp fit - pre-fit
+% tmp_guess = [signal(1) 1e-9];
+% tmp_lb = [.9*signal(1) 1e-11];
+% tmp_ub = [1.1*signal 3e-9];
+% index = find(xps.mde_b1 == 0);
+% Xin = xps.mde_b1(index);
+% Xin = Xin(opt.nbstart:end);
+% Yin = signal(index);
+% Yin = Yin(opt.nbstart:end);
+
+%Xin = xps.mde_b2;
+
+%Xin = Xin(opt.nbstart:end);
+% Yin = signal(index);
+% Yin = Yin(opt.nbstart:end);
+
+%%%%%%%%%%% for pre-fitting
+% function Y = fexp(Pin,Xin,Pnorm,Xnorm,Ynorm);
+%
+% if nargin == 2
+%     Pnorm = ones(size(Pin));
+%     Xnorm = 1;
+%     Ynorm = 1;
+% end
+%
+% Pin = Pin.*Pnorm;
+% Xin = Xin*Xnorm;
+%
+% Y0 = Pin(1);
+% D = Pin(2);
+%
+% Y = Y0.*exp( - D.*Xin);
+% Y = Y/Ynorm;
+end
+
+
+
---
+++ models/fexi11/fexi11_4d_data2fit.m
@@ -0,0 +1,49 @@
+function mfs_fn = fexi11_4d_data2fit(s, o, opt)
+% function mfs_fn = fexi11_4d_data2fit(s, o, opt)
+%
+% Loops over a 4D volume to produce fit parameters with the FEXI nls model
+%
+% Input: 
+%
+% s   - data structure
+%          s.nii_fn - full path to nifti filename with data
+%          s.xps    - experimental parameter structure
+%
+% o   - output folder   
+% 
+% opt - options structure, optional
+%
+% Output:
+%
+% mfs_fn - path to .mat file with the ModelFitStructure (mfs)
+
+if (nargin < 3), opt = []; end
+
+opt = fexi11_opt(opt);
+
+ind = s.xps.mde_b2_ind >= opt.fexi11.mde_b2_ind_start;
+
+    function m = my_core(signal, do_fit)
+        
+        if (do_fit)
+            m = fexi11_1d_data2fit(signal, s.xps, opt, ind);
+        else
+            m = zeros(1,3+max(s.xps.s_ind));
+        end
+        
+        if (opt.fexi11.do_plot)
+            signal_fit = fexi11_1d_fit2data(m, s.xps);
+            x = (1:numel(signal))';
+            plot(x,signal,'.',x,signal_fit,'o');
+            pause(0.05);
+        end
+        
+   end
+
+% Verify the xps
+fexi11_check_xps(s.xps);
+
+% Loop over the volume and fit the model
+mfs_fn = mio_fit_model(@my_core, s, o, opt);
+
+end
