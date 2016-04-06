7828c560f3158ceaf9ba2c7ed7b0db8d45337375
Markus Nilsson <nilzzon@gmail.com>
2016-04-06 16:47:08 +0200

    cleanup for the fexi model

--- models/fexi11/fexi11_check_xps.m
+++ models/fexi11/fexi11_check_xps.m
@@ -6,7 +6,11 @@ function fexi11_check_xps(xps)
 mdm_xps_check(xps);
 
 required_fields = {...
-    'mde_b1', 'mde_b2', 'mde_tm12', 's_ind', 'mde_b2_ind'};
+    'mde_b1', ...   % filter b-value
+    'mde_b2', ...   % detection b-value
+    'mde_tm12', ... % mixing time from a diffusion point of view
+    's_ind', ...    % series index, yields separate s0's
+    'mde_b2_ind'};  % unqiue detection b-values, for guessing s0
 
 for c = 1:numel(required_fields)
     if (~isfield(xps, required_fields{c}))
