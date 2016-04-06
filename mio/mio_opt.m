0ea942c04681cafea403485ac2f09414a3297221
Markus Nilsson <nilzzon@gmail.com>
2016-04-06 16:46:03 +0200

    adding support for another variable in the volume loop

--- mio/mio_opt.m
+++ mio/mio_opt.m
@@ -1,10 +1,13 @@
 function opt = mio_opt(opt)
 % function opt = mio_opt(opt)
 
+if (nargin < 1), opt = []; end
+
 opt = mdm_opt(opt);
 
 opt.mio.present = 1;
 
+opt.mio = msf_ensure_field(opt.mio, 'no_parfor', 0); 
 
 opt.mio.coreg.present = 1;
 opt.mio.coreg = msf_ensure_field(opt.mio.coreg, 'clear_header', 1);
--- mio/ut_mio.m
+++ mio/ut_mio.m
@@ -20,4 +20,41 @@ switch (c_ut)
         
         if (~all(size(I3) == size(I))), error('%s, ut_mio test %i, step2', fn, c_ut); end
         
+    case {2,3,4,5}
+        fn = 'mio_volume_loop.m';
+        
+        opt.verbose = 0;
+        
+        I = ones(11,12,13,14);
+        M = I(:,:,:,1) > 0;
+        
+        % allow test with and without parpool
+        try % start parpool outside this function to have it run in parfor mode
+            tmp = gcp('nocreate');
+            n_workers = tmp.NumWorkers;
+        catch
+            n_workers = 1;
+        end
+        
+        switch (c_ut)
+            case 2
+                opt.mio.no_parfor = 1;
+                P = mio_volume_loop(@sum, I, M, opt);
+                if (~all(P(:) == 14)), error('%s, ut_mio test %i', fn, c_ut); end
+            case 3
+                opt.mio.no_parfor = 1;
+                P = mio_volume_loop(@(x,y) sum(x) + sum(y), I, M, opt, I);
+                if (~all(P(:) == 14*2)), error('%s, ut_mio test %i', fn, c_ut); end
+            case 4
+                if (n_workers == 1), error('%s, ut_mio test %i; run parpool(x) first to test such functions', fn, c_ut); end
+                P = mio_volume_loop(@sum, I, M, opt);
+                if (~all(P(:) == 14)), error('%s, ut_mio test %i', fn, c_ut); end
+                
+            case 5
+                if (n_workers == 1), error('%s, ut_mio test %i; run parpool(x) first to test such functions', fn, c_ut); end
+                P = mio_volume_loop(@(x,y) sum(x) + sum(y), I, M, opt, I);
+                if (~all(P(:) == 14*2)), error('%s, ut_mio test %i', fn, c_ut); end
+                
+        end
+        
 end
