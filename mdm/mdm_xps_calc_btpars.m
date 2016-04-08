dc06e3fcf8c9ef8ea6867a88532bc95f7ae39b74
Markus Nilsson <nilzzon@gmail.com>
2016-04-08 20:22:46 +0200

    Cleaning up xps and bt parts code

---
+++ mdm/mdm_xps_calc_btpars.m
@@ -0,0 +1,14 @@
+function xps = mdm_xps_calc_btpars(xps)
+% function xps = mdm_xps_calc_btpars(xps)
+
+for c = 1:xps.n
+    t = tm_1x6_to_3x3(xps.bt(c,:));
+    
+    t = tm_t2tpars(t);
+    
+    f = fieldnames(t);
+    for c_field = 1:numel(f)
+        xps.(['b_' f{c_field}])(c,:) = t.(f{c_field});
+    end
+    
+end
