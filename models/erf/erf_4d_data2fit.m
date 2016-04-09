1b865b46d20618126a598c60a8059fd2174bed81
Markus Nilsson <nilzzon@gmail.com>
2016-04-10 00:40:56 +0200

    correcting bug in erf_4d_data2fit

--- models/erf/erf_4d_data2fit.m
+++ models/erf/erf_4d_data2fit.m
@@ -1,4 +1,4 @@
-function erf_4d_data2fit(s, mfs_fn, opt)
+function msf_fn = erf_4d_data2fit(s, mfs_fn, opt)
 % function erf_4d_data2fit(s, mfs_fn, opt)
 %
 % Error function fit
@@ -6,16 +6,10 @@ function erf_4d_data2fit(s, mfs_fn, opt)
 % Eriksson et al., J. Chem. Phys. 142, 104201 (2015).
 % http://dx.doi.org/10.1063/1.4913502
 
-
 if (nargin < 3), opt = []; end
 
-ind = 1:s.xps.n;
-
-%Verify the xps
-%dti_euler_mic_check_xps(s.xps);
-
 % Loop over the volume and fit the model
-xps = s.xps; % this appears to improve parallel performance
-f = @(signal) erf_1d_data2fit(signal, xps, opt, ind);
-dummy = mio_fit_model(f, s, mfs_fn, opt);
+xps     = s.xps; % this appears to improve parallel performance
+f       = @(signal) erf_1d_data2fit(signal, xps, opt);
+msf_fn  = mio_fit_model(f, s, mfs_fn, opt);
 
