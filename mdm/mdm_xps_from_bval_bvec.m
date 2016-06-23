dfc80a0e777050dd967c36b1fe1150855fd44326
Markus Nilsson <nilzzon@gmail.com>
2016-06-23 15:14:06 +0200

    fixes

--- mdm/mdm_xps_from_bval_bvec.m
+++ mdm/mdm_xps_from_bval_bvec.m
@@ -1,6 +1,14 @@
 function xps = mdm_xps_from_bval_bvec(bval_fn, bvec_fn)
 % function xps = mdm_xps_from_bval_bvec(bval_fn, bvec_fn)
 
+if (nargin == 1) % assume it is a nifti filename
+    [nii_path, nii_name] = msf_fileparts(bval_fn);
+    bval_fn = fullfile(nii_path, [nii_name '.bval']);
+    bvec_fn = fullfile(nii_path, [nii_name '.bvec']);
+end
+
+if (~exist(bval_fn, 'file')), error('could not find %s', bval_fn); end
+
 bval = mdm_txt_read(bval_fn);
 xps.b = str2num(bval{1})' * 1e6; 
 xps.n = numel(xps.b);
