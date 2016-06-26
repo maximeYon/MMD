89bae446b5c7117759edbc511bc5df7d9bce038c
Markus Nilsson <nilzzon@gmail.com>
2016-06-26 19:08:59 +0200

    tensor maths: updating function names

---
+++ tools/tensor_maths/tm_1x6_to_tpars.m
@@ -0,0 +1,6 @@
+function t = tm_1x6_to_tpars(t_1x6)
+% function t = tm_1x6_to_tpars(t_1x6)
+
+t = tm_3x3_to_tpars(tm_1x6_to_3x3(t_1x6));
+
+
--- tools/tensor_maths/tm_t2tpars.m
+++ tools/tensor_maths/tm_t2tpars.m
@@ -3,46 +3,8 @@ function t = tm_t2tpars(t3x3)
 %
 % calculate derived tensor parameters
 
-if (nargin < 2), t = []; end
+warning('lecacy code');
+t = tm_3x3_to_tpars(t3x3);
 
-t.t1x6 = tm_3x3_to_1x6(t3x3);
-[V,D] = eig(t3x3);
-lambdas = diag(D);
-t.trace = sum(lambdas);
-t.iso = t.trace/3;
-[dummy,ind] = sort(lambdas,'descend');
-t.lambda33 = lambdas(ind(1));                        
-t.lambda22 = lambdas(ind(2));                       
-t.lambda11 = lambdas(ind(3));  
-t.lambda33vec = V(:,ind(1))';
-t.lambda22vec = V(:,ind(2))';
-t.lambda11vec = V(:,ind(3))';
-Dlambdas = abs(lambdas-t.iso);
-[dummy,ind] = sort(Dlambdas,'descend');
-t.lambdazz = lambdas(ind(1));
-t.lambdaxx = lambdas(ind(2));                        
-t.lambdayy = lambdas(ind(3));
-t.lambdazzvec = V(:,ind(1))';
-t.lambdaxxvec = V(:,ind(2))';
-t.lambdayyvec = V(:,ind(3))';
 
-t.vlambda = 1/3*((t.lambda33 - t.iso).^2 + (t.lambda22 - t.iso).^2 + (t.lambda11 - t.iso).^2);
-t.cm = t.vlambda./t.iso.^2;
-t.cm(isnan(t.cm)) = 0;
-t.delta = (t.lambdazz - (t.lambdayy+t.lambdaxx)/2)./t.trace;
-t.eta = 3*(t.lambdayy - t.lambdaxx+eps)./(2*t.trace.*t.delta+eps);
-t.l = t.lambda33 - t.lambda22;
-t.p = 2*(t.lambda22 - t.lambda11);
-t.s = 3*t.lambda11;
-
-t.fa = sqrt(1/2)*sqrt((t.lambda33-t.lambda22).^2+(t.lambda33-t.lambda11).^2+(t.lambda22-t.lambda11).^2)...
-                    ./sqrt(t.lambda33.^2+t.lambda22.^2+t.lambda11.^2);
-t.fa(isnan(t.fa)) = 0;
-
-t.cl = t.l./t.trace;
-t.cl(isnan(t.cl)) = 0;
-t.cp = t.p./t.trace;
-t.cp(isnan(t.cp)) = 0;
-t.cs = t.s./t.trace;
-t.cs(isnan(t.cs)) = 0;
 
