308743211a0942f3a5a900154c3763fe4ba02145
Markus Nilsson <nilzzon@gmail.com>
2016-07-06 12:51:55 +0200

    Fixing the resize bug

--- tools/mgui/mgui_analysis_setup_gui.m
+++ tools/mgui/mgui_analysis_setup_gui.m
@@ -39,4 +39,45 @@ EG.handles.h_analysis_popup = uicontrol('style','popupmenu', ...
     'callback', @mgui_roi_gui_callback);
 
 
-1;
+
+
+
+% Handle resizing: Warning for ugly code, needs rethink (XXX)
+set(h, 'ResizeFcn', @mgui_browse_resize);
+t_fixed_top = {...
+    EG.t_ANALYSIS_POPUP};
+
+h_fixed_top = [];
+for c = 1:numel(t_fixed_top)
+    h_fixed_top = [h_fixed_top findobj('Tag', t_fixed_top{c})];
+end
+tmp_p1 = get(h_fixed_top(1), 'Position');
+tmp_p2 = get(h, 'Position');
+d_fixed_top = tmp_p2(4) - tmp_p1(2);
+
+
+    function mgui_browse_resize(h,varargin)
+        
+        if (~exist('h_fixed_top', 'var')), return; end
+        
+        % Ensure we're working in pixels and get the position
+        old_units = get(h,'Units');
+        set(h,'Units','pixels');
+        p_parent = get(h,'Position');
+        
+        % Keep some elements at top
+        for h_elem = h_fixed_top
+            p = get(h_elem, 'position');
+            p(2) = p_parent(4) - d_fixed_top;
+            set(h_elem, 'position', p);
+        end
+        
+        % Enlarge? 
+        % later
+
+        % Restore old units
+        set(h,'Units',old_units);
+    end
+
+end
+
