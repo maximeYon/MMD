d9aa77de32e97068bd4c94ebae6d248389380cf5
Markus Nilsson <nilzzon@gmail.com>
2016-07-14 10:39:54 +0200

    mdm fixes

--- mdm/mdm_xps_check.m
+++ mdm/mdm_xps_check.m
@@ -9,6 +9,8 @@ if (~isfield(xps,'n')), error('xps.n is required'); end
 f = fieldnames(xps);
 for c = 1:numel(f)
     if (strcmp(f{c}, 'n')), continue; end
+    if (strcmp(f{c}, 'intent')), continue; end
+    if (strcmp(f{c}, 'c_volume')), continue; end
     
     if (size(xps.(f{c}), 1) ~= xps.n)
         error('field %s is of wrong size (%s)', f{c}, num2str(size(f{c}))); 
