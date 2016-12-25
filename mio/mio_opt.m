df5691dc3333d4bd7f2605b1ba456d235780512e
Markus Nilsson <nilzzon@gmail.com>
2016-12-25 12:57:54 +0100

    fixing merge issue

--- mio/mio_opt.m
+++ mio/mio_opt.m
@@ -15,13 +15,12 @@ opt.mio.coreg = msf_ensure_field(opt.mio.coreg, 'assume_las', 1);
 opt.mio.coreg = msf_ensure_field(opt.mio.coreg, 'pad_xyz', [0 0 0]);
 opt.mio.coreg = msf_ensure_field(opt.mio.coreg, 'adjust_intensity', 0);
 
-<<<<<<< 8b18569fffa7e4ba2b8efe91eef877661e6fea47
 
 
 opt.mask.present = 1;
 opt.mask = msf_ensure_field(opt.mask, 'pca_threshold', 1.3);
-=======
+
 opt.mio.pa.present = 1;
 opt.mio.pa = msf_ensure_field(opt.mio.pa, 'do_abs', 1);
 opt.mio.pa = msf_ensure_field(opt.mio.pa, 'method', 'ari');
->>>>>>> powder averaging (pa): improving structure
+
