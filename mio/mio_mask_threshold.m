6df42268ed9a4acec2e70f6c808ec3cc100a45e9
Daniel Topgaard <daniel.topgaard@fkem1.lu.se>
2017-02-27 14:41:57 +0100

    mio_mask_threshold bug-fix
    Fixing leftovers from “thresh” to “threshold” name change. Modifying
    mio_mask_threshold to operate also on complex data.

--- mio/mio_mask_threshold.m
+++ mio/mio_mask_threshold.m
@@ -22,6 +22,9 @@ opt = mio_opt(opt);
 if (nargin < 3), threshold = opt.mask.threshold; end
 if (nargin < 4), ind = opt.mask.b0_ind; end
 
+% Disallow model fits to complex data
+if (any(imag(I) ~= 0)), I = abs(I); end 
+
 % define the mask from threshold of b0 image
 I0 = mean(I(:,:,:,ind),4);
 
