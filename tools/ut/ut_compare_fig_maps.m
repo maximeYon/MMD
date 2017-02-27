8aaca6d9b8de4a32d8976a73afd5f811c397894f
Markus Nilsson <nilzzon@gmail.com>
2017-02-27 23:02:08 +0100

    Enable inspection of difference volumes

--- tools/ut/ut_compare_fig_maps.m
+++ tools/ut/ut_compare_fig_maps.m
@@ -1,5 +1,16 @@
-function ut_compare_fig_maps(a, b, k, method)
+function ut_compare_fig_maps(a, b, k, method, do_show)
 % function ut_compare_fig_maps(a, b, k, method)
+%
+% Compare image volumes in folders 'a' and 'b', in transversal slice 'k'
+% 
+% The maps that are obtained are fetched from opt.(method).fig_maps.
+%
+% Optional
+% do_show - show actual image maps (with pause, disables errors)
+%           
+
+
+if (nargin < 5), do_show = 0; end
 
 opt = feval([method '_opt']); 
 
@@ -25,12 +36,50 @@ for c = 1:numel(opt.(method).fig_maps)
     A = A(:,:,k);
     B = B(:,:,k);
     
+    A(isnan(A(:))) = 0;
+    B(isnan(B(:))) = 0;
+    
     % compare the two: allow a difference of 1% of max image value in at
     % most 1% of all voxels
     n = sum( abs(A(:) - B(:)) > 1e-2 * max(q(A),q(B)));
     n_tol = 0.01 * sum( (A(:) ~= 0) & (B(:) ~= 0) );
     if (n > n_tol)
-        error('%s, %s, analysed file differs (%s)', mfilename, method, map);
+        
+        if (do_show)
+            f = @(varargin) warning(varargin{:});
+        else
+            f = @(varargin) error(varargin{:});
+        end
+        
+        f('%s, %s, analysed file differs (%s)', mfilename, method, map);
     end        
+    
+    
+    if (do_show)
+        
+        c_min = min(min(A(:)), min(B(:)));
+        c_min = min(0, c_min);
+        c_max = max(quantile(A(:), 0.99), quantile(B(:), 0.99));
+        
+        subplot(2,2,1);
+        msf_imagesc(A);
+        caxis([c_min c_max]);
+        colorbar;
+        title(strrep(p_fn, '_', '\_'));
+        
+        subplot(2,2,2);
+        msf_imagesc(B);
+        caxis([c_min c_max]);
+        colorbar;
+        
+        subplot(2,2,3);
+        msf_imagesc(A-B);
+        caxis([-1 1] * max(abs([c_min c_max])) * 0.1);
+        colorbar;
+        
+        pause;
+    end
+        
+        
     
 end
