30d267735658e30adf00e93cb0ec76e5d1871abf
Filip Szczepankiewicz <filip.szczepankiewicz@gmail.com>
2017-02-27 10:46:57 +0100

    Further improvements to variable baseline signal. This version works with current plotting code and makes minimal assumptions on the xps.
    Signed-off-by: Filip Szczepankiewicz <filip.szczepankiewicz@gmail.com>

--- methods/dtd_gamma/dtd_gamma_1d_fit2data.m
+++ methods/dtd_gamma/dtd_gamma_1d_fit2data.m
@@ -2,16 +2,18 @@ function s = dtd_gamma_1d_fit2data(m, xps)
 % function s = dtd_gamma_1d_fit2data(m, xps)
 
 % convert to readable parameters
-s0        = m(1);
-d_iso     = m(2);
-mu2_iso   = m(3);
-mu2_aniso = m(4);
+s0        = m(1);         % Signal at no diff encoding (baseline signal)
+d_iso     = m(2);         % Average isotropic diffusivity (mean diffusivity)
+mu2_iso   = m(3);         % Isotropic variance
+mu2_aniso = m(4);         % Anisotropic variance
+
+rs        = [1 m(5:end)]; % Relative signal (Series 1, 2, 3, ...)
 
 % signal baseline coefficient that adjusts for variable baseline signal
 % across multiple series (xps.s_ind).
-sw = s0 * sum( mtimes(ones(xps.n,1), [1 m(5:end)]) .* ...
-    (xps.s_ind == 1:(numel(m([1 5:end])))) , 2);
-
+sw = s0 * sum(  mtimes(  ones(size(xps.b)), rs    ) .* ...
+                      (  xps.s_ind == 1:numel(rs) )  , 2);
+                               
 % Total diffusional variance (mu2) is the weighted sum of the isotropic 
 % (mu2_iso) and anisotorpic (mu2_aniso) components.
 mu2 = mu2_iso + mu2_aniso.*xps.b_delta.^2.*(xps.b_eta.^2+3)/3;
