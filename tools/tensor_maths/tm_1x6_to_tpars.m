a17ef2fda171b9023e5e32c5985b6b401f4620ea
Markus Nilsson <nilzzon@gmail.com>
2017-03-07 10:33:41 +0100

    Tensor maths improvements for DKI

--- tools/tensor_maths/tm_1x6_to_tpars.m
+++ tools/tensor_maths/tm_1x6_to_tpars.m
@@ -1,6 +1,9 @@
 function t = tm_1x6_to_tpars(t_1x6)
 % function t = tm_1x6_to_tpars(t_1x6)
 
+if (size(t_1x6,2) ~= 6), error('wrong dimension of tensor'); end
+
+
 t = tm_3x3_to_tpars(tm_1x6_to_3x3(t_1x6));
 
 
