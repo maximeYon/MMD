862f81a923c185799abe5926e4432c37672a2140
Filip Szczepankiewicz <filip.szczepankiewicz@gmail.com>
2017-03-17 16:26:25 +0100

    Update gamma model to support negative variance
    To get distribution of variances symmetric expectancy near zero the
    variances can now be negative. However, the signal is constrined to be
    real.
    Added repeated random guess to avoid extremely poor fitting results due
    to very bad guesses.
    Fixerd bugg so that random guess is updated between iterations
    Updated guess in second fitting (if using weights)
    Adde UT for negative variances

--- methods/dtd_gamma/dtd_gamma_1d_data2fit.m
+++ methods/dtd_gamma/dtd_gamma_1d_data2fit.m
@@ -1,7 +1,8 @@
 function m = dtd_gamma_1d_data2fit(signal, xps, opt, ind)
 % function m = dtd_gamma_1d_data2fit(signal, xps, opt, ind)
 %
-% m = [s0 d_iso mu2_iso mu2_aniso]
+% m = [s0   d_iso mu2_iso mu2_aniso]
+% m = [s0   MD    Vi      Va       ]
 
 if (nargin < 4), ind = ones(size(signal)) > 0; end
 
@@ -11,49 +12,40 @@ else
     ns = 0;
 end
 
-
 unit_to_SI = [max(signal+eps) 1e-9 (1e-9)^2*[1 1] ones(1,ns)];
 
-
-    function m = t2m(t) % convert local params to outside format
-        
+% Convert local params to outside format.
+    function m = t2m(t)
         % define model parameters
         s0          = t(1);
         d_iso       = t(2);
         mu2_iso     = t(3);
         mu2_aniso   = t(4);
-        
         sw          = t((end-(ns - 1)):end);
-        
         m = [s0 d_iso mu2_iso mu2_aniso sw] .* unit_to_SI;
-        
     end
 
+% Convert non weighted to weighted signal.
     function s = my_1d_fit2data(t,varargin)
-        
         m = t2m(t);
-        
         % signal
         s = dtd_gamma_1d_fit2data(m, xps);
         s = s(ind).*weight(ind);
-        
     end
 
-    % soft heaviside weighting function
-    % limit the fit to the initial slope
-    % sthresh: normalized signal threshold value [0.2]
-    % mdthresh: MD [1e-9]
-    % wthresh: width of transition from 1 to 0 [5]
-    % bthresh: b-value at transition
+% Soft heaviside weighting function
+% limit the fit to the initial slope
+% sthresh: normalized signal threshold value [0.2]
+% mdthresh: MD [1e-9]
+% wthresh: width of transition from 1 to 0 [5]
+% bthresh: b-value at transition
     function weight = weightfun(sthresh,mdthresh,wthresh)
-        
         bthresh = -log(sthresh)/mdthresh;
         weight = .5*(1-erf(wthresh*(xps.b - bthresh)/bthresh));
-        
     end
 
-    % Weight function that corrects for hetrascedacity due to different number
-    % of images being averaged in the powder avereaged signal
+% Weight function that corrects for hetroscedacity due to different number
+% of images being averaged in the powder avereaged signal
     function w = calc_weight_from_signal_samples()
         if ~isfield(xps, 'pa_w')
             w = ones(size(xps.b));
@@ -62,40 +54,62 @@ unit_to_SI = [max(signal+eps) 1e-9 (1e-9)^2*[1 1] ones(1,ns)];
         end
     end
 
+% Create 100 random guesses and use the one with lowest residual. This
+% function is reasonably fast, and avoids strange fitting results due to
+% poor initial guessing.
+    function guess = dtd_gamma_rand_guess(lb, ub)
+        iter = 100;
+        thr = inf;
+        for j = 1:iter
+            m_rand = lb + (ub - lb) .* rand(size(lb));
+            s_rand = dtd_gamma_1d_fit2data(m_rand, xps);
+            r_rand = sum(((signal-s_rand).*weight).^2);
+            if r_rand < thr
+                thr = r_rand;
+                guess = m_rand;
+            end
+        end
+    end
+
 
 % Guesses and bounds
-m_lb      = [0                  1e-12          0*[1 1]   0.5 * ones(1,ns)];
-m_ub      = [2*max(signal+eps)  4e-9    (3e-9)^2*[1 1]   2.0 * ones(1,ns)];
+m_lb      = [0                   1e-12   -(3e-9)^2*[1 1]   0.5 * ones(1,ns)];
+m_ub      = [2*max(signal+eps)   4e-9     (3e-9)^2*[1 1]   2.0 * ones(1,ns)];
 
-m_guess   = m_lb + (m_ub - m_lb) .* rand(size(m_lb));
+m_lbz     = m_lb .* (m_lb > 0); % Avoid negative guess
 
-t_guess   = m_guess./unit_to_SI;
 t_lb      = m_lb./unit_to_SI;
 t_ub      = m_ub./unit_to_SI;
-
-
-% initial fit with weighting using guess value of MD
-weight = ones(xps.n,1);
-if (opt.dtd_gamma.do_weight)
-    weight = weightfun(opt.dtd_gamma.weight_sthresh,opt.dtd_gamma.weight_mdthresh,opt.dtd_gamma.weight_wthresh);
-end
-
-% Weight with 1/sqrt(n) so that LS-fit is weighted to 1/n propto 1/variance
-if (opt.dtd_gamma.do_pa_weight)
-    weight = weight .* calc_weight_from_signal_samples();
-end
-
 
 r_thr = inf;
 
 for i = 1:opt.dtd_gamma.fit_iters
     
+    % initial fit with weighting using guess value of MD
+    weight = ones(xps.n,1);
+    
+    if (opt.dtd_gamma.do_weight)
+        weight = weightfun(opt.dtd_gamma.weight_sthresh,opt.dtd_gamma.weight_mdthresh,opt.dtd_gamma.weight_wthresh);
+    end
+    
+    % Weight with 1/sqrt(n) so that LS-fit is weighted to 1/n propto 1/variance
+    if (opt.dtd_gamma.do_pa_weight)
+        weight = weight .* calc_weight_from_signal_samples();
+    end
+    
+    % Create a random guess
+    m_guess = dtd_gamma_rand_guess(m_lbz, m_ub);
+    t_guess = m_guess./unit_to_SI;
+    
+    
+    % Non-linear fit to data
     t = lsqcurvefit(@my_1d_fit2data, t_guess, [], signal(ind).*weight(ind), t_lb, t_ub,...
         opt.dtd_gamma.lsq_opts);
     
     m = t2m(t);
     
-    % redo the fit with updated value of MD
+    % Redo the fit with weighting based on attenuation (and updated
+    % estimate of MD).
     if (opt.dtd_gamma.do_weight)
         weight = weightfun(opt.dtd_gamma.weight_sthresh,m(2),opt.dtd_gamma.weight_wthresh);
         
@@ -103,7 +117,7 @@ for i = 1:opt.dtd_gamma.fit_iters
             weight = weight .* calc_weight_from_signal_samples();
         end
         
-        t = lsqcurvefit(@my_1d_fit2data, t_guess, [], signal(ind).*weight(ind), t_lb, t_ub,...
+        t = lsqcurvefit(@my_1d_fit2data, t, [], signal(ind).*weight(ind), t_lb, t_ub,...
             opt.dtd_gamma.lsq_opts);
         
         m = t2m(t);
@@ -122,9 +136,7 @@ for i = 1:opt.dtd_gamma.fit_iters
     
 end
 
-
 m = m_keep;
-
 
 if (opt.dtd_gamma.do_plot)
     signal_fit = dtd_gamma_1d_fit2data(m, xps);
