6c13ae6fc7552dd1c2db2aafc276ead733dc88a3
Markus Nilsson <nilzzon@gmail.com>
2017-03-22 17:26:54 +0100

    Improving the gui code

--- tools/mgui/mgui_analysis_update_panel.m
+++ tools/mgui/mgui_analysis_update_panel.m
@@ -1,124 +1,132 @@
-function EG = mgui_analysis_update_panel(EG, do_reload, do_update)
-% function EG = mgui_analysis_update_panel(EG, S)
+function EG = mgui_analysis_update_panel(EG, varargin) 
+% function EG = mgui_analysis_update_panel(EG, do_reload, do_update)
 
-if (nargin < 2), do_reload = 1; end;
-if (nargin < 3), do_update = 1; end
+% currently, we don't separate reload and update
+% if (nargin < 2), do_reload = 1; end;
+% if (nargin < 3), do_update = 1; end
 
 EG.roi = msf_ensure_field(EG.roi, 'xps', []);
 
-
-h_top = EG.handles.h_analysis_top_axes;
+% get handles
+h_top    = EG.handles.h_analysis_top_axes;
 h_bottom = EG.handles.h_analysis_bottom_axes;
 h_popup  = EG.handles.h_analysis_popup;
 
 
 % repopulate the model selection dropdown
-if (do_reload)
-    
-    % get current selection
-    s = get(h_popup, 'String');
-    c = get(h_popup, 'Value');
-    try
-        current_selection = s{c};
-    catch
-        current_selection = {};
-    end
-    
-    
-    d = dir(fullfile(fileparts(mfilename('fullpath')), '..', '..','methods'));
-    
-    str = {'Overview'};
-    for c = 1:numel(d)
-        if (d(c).name(1) ~= '.') && (d(c).isdir)
-            
-            f_name = [d(c).name '_check_xps'];
-            
-            try
-                % currently the check xps functions throw errors
-                feval(f_name, EG.roi.xps);
-                str{end+1} = d(c).name;
 
-            catch me
-                if (0)
-                    disp(me.message);
-                end
-            end
-        end
-    end
-    
-    set(h_popup, 'String', str);
-    
-    value = -1;
-    for c = 1:numel(str)
-        if (strcmp(str{c}, current_selection))
-            value = c;
-            break;
-        end
-    end
-    
-    if (value > -1)
-        set(h_popup, 'value', value);
-    end
-    
-    if (get(h_popup, 'value') > numel(str))
-        set(h_popup, 'value', 1);
-    end
-    
-    
-    
-    value = get(h_popup, 'value');
-    switch (value);
-        case 1 % show the default overview 1D or 2D
-            if (size(EG.roi.I,4) == 1)
-                f_plot = @(S, xps) plot_histogram(S, xps, h_top, h_bottom);
-            else
-                f_plot = @(S, xps) mgui_analysis_plot_overview(S, xps, h_top, h_bottom);
-            end
-        otherwise
-            f_plot = @(S,xps) mgui_analysis_plot(str{value}, S, xps, h_top, h_bottom); 
-    end
-                
-    
-    
-
+% get current selection
+s = get(h_popup, 'String');
+c = get(h_popup, 'Value');
+try
+    current_selection = s{c};
+catch
+    current_selection = {};
 end
 
-% update the model fits et c
-if (do_update)
-    
-    % pull out signal
-    if (msf_isfield(EG, 'roi') && ...
-            msf_isfield(EG.roi, 'I') && ...
-            msf_isfield(EG.roi, 'I_roi'))
+% Check which methods that are available
+d = dir(fullfile(fileparts(mfilename('fullpath')), '..', '..','methods'));
+
+% Set default method
+str = {'Overview'};
+
+% Populate popup with additional methods
+for c = 1:numel(d)
+    if (d(c).name(1) ~= '.') && (d(c).isdir)
         
-        if (~isfield(EG,'analysis') || ~isfield(EG.analysis, 'S'))
-            S = zeros(sum(EG.roi.I_roi(:)), size(EG.roi.I,4));
-            for c = 1:size(EG.roi.I,4)
-                tmp = EG.roi.I(:,:,:,c);
-                S(:,c) = tmp(EG.roi.I_roi(:) > 0);
-            end
+        f_name = [d(c).name '_check_xps'];
+        
+        try
+            % currently the check xps functions throw errors
+            feval(f_name, EG.roi.xps);
+            str{end+1} = d(c).name;
             
-            EG.analysis.S = S;
+        catch me
+            % show that the method does not work with an (x) appended
+            % to the method name
+            str{end+1} = [ d(c).name ' (x)'];
+            
+            if (0)
+                disp(me.message);
+            end
+        end
+    end
+end
+
+set(h_popup, 'String', str);
+
+% Determine the selected method
+value = -1;
+for c = 1:numel(str)
+    if (strcmp(str{c}, current_selection))
+        value = c;
+        break;
+    end
+end
+
+% make sure the selection and the determined method is consistent
+if (value > -1)
+    set(h_popup, 'value', value);
+end
+
+if (get(h_popup, 'value') > numel(str))
+    set(h_popup, 'value', 1);
+end
+
+c_method = get(h_popup, 'value');
+
+
+% connect plot function to method
+switch (c_method)
+    
+    case 1 % show the default overview 1D or 2D
+        if (size(EG.roi.I,4) == 1)
+            f_plot = @(S, xps) plot_histogram(S, xps, h_top, h_bottom);
         else
-            S = EG.analysis.S;
+            f_plot = @(S, xps) mgui_analysis_plot_overview(...
+                S, xps, h_top, h_bottom);
         end
         
+    otherwise % run custom plot scripts
+        f_plot = @(S,xps) mgui_analysis_plot(str{c_method}, ...
+            S, xps, h_top, h_bottom);
+end
+
+
+
+% pull out signal
+if (msf_isfield(EG, 'roi') && ...
+        msf_isfield(EG.roi, 'I') && ...
+        msf_isfield(EG.roi, 'I_roi'))
+    
+    % only load the signal once
+    if (~isfield(EG,'analysis') || ~isfield(EG.analysis, 'S'))
+        S = zeros(sum(EG.roi.I_roi(:)), size(EG.roi.I,4));
+        for c = 1:size(EG.roi.I,4)
+            tmp = EG.roi.I(:,:,:,c);
+            S(:,c) = tmp(EG.roi.I_roi(:) > 0);
+        end
+        
+        EG.analysis.S = S;
     else
-        S = [];
+        S = EG.analysis.S;
     end
     
-    % clear content
-    cla(h_top,'reset');
-    cla(h_bottom,'reset');
-    
-    
-    xps = EG.roi.xps;
-    xps.c_volume = EG.roi.c_volume; % bit nasty, but...
-    
-    % run analysis and/or plot script
-    f_plot(S, xps);
-        
+else
+    S = [];
 end
+
+% clear content
+cla(h_top,'reset');
+cla(h_bottom,'reset');
+
+
+xps = EG.roi.xps;
+xps.c_volume = EG.roi.c_volume; % bit nasty, but...
+
+% run analysis and/or plot script
+f_plot(S, xps);
+
 
     function plot_histogram(S, xps, h_top, h_bottom)
         
@@ -128,5 +136,5 @@ end
         
     end
 
-
 end
+
