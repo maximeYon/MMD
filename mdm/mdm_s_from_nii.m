5109b0b09d5061438342943a094b1dd01008c898
Markus Nilsson <nilzzon@gmail.com>
2017-04-12 16:55:25 +0200

    Housekeeping fixes

--- mdm/mdm_s_from_nii.m
+++ mdm/mdm_s_from_nii.m
@@ -9,4 +9,9 @@ function s = mdm_s_from_nii(nii_fn)
 % assumes the xps filename can be constructred from the nii_fn
 
 s.nii_fn = nii_fn;
-s.xps    = mdm_xps_load(mdm_xps_fn_from_nii_fn(nii_fn));
+
+if (exist(mdm_xps_fn_from_nii_fn(nii_fn), 'file'))
+    s.xps = mdm_xps_load(mdm_xps_fn_from_nii_fn(nii_fn));
+elseif (exist(mdm_fn_nii2gdir(nii_fn), 'file'))
+    s.xps = mdm_xps_from_gdir(mdm_fn_nii2gdir(nii_fn));
+end
