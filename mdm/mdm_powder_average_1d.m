d8e9d19569f789e36f6b6d1a647df2192910e844
Markus Nilsson <nilzzon@gmail.com>
2017-11-18 14:01:58 +0100

    Bugfixes

--- mdm/mdm_powder_average_1d.m
+++ mdm/mdm_powder_average_1d.m
@@ -1,9 +1,9 @@
-function [signal_pa,xps_pa] = mdm_powder_average_1d(signal,xps)
+function [signal_pa,xps_pa] = mdm_powder_average_1d(signal,xps,opt)
 % function [signal_pa,xps_pa] = mdm_powder_average_1d(signal,xps)
 %
 % Returns a powder-average of a 1d signal
 
-[~,c_list, id_ind] = mdm_pa_ind_from_xps(xps);
+[~,c_list, id_ind] = mdm_pa_ind_from_xps(xps,opt);
 
 signal_pa = zeros(numel(c_list), 1);
 for c = c_list'
@@ -11,4 +11,4 @@ for c = c_list'
 end
 
 % Average fields in xps
-xps_pa = mdm_xps_pa(xps);
+xps_pa = mdm_xps_pa(xps,opt);
