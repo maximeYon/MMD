3c496f176bb92258db3013126db70f3db5076169
Filip Szczepankiewicz <filip.szczepankiewicz@gmail.com>
2018-02-27 14:07:46 +0100

    Tiny update to function to make it more secure

--- tools/gwf/gwf_maxwell_index.m
+++ tools/gwf/gwf_maxwell_index.m
@@ -1,6 +1,14 @@
 function m = gwf_maxwell_index(gwf, rf, dt)
 % function m = gwf_maxwell_index(gwf, rf, dt)
 
+% gwf must be a nx3 matrix that defines directions along the time
+% dimension.
+% rf must be a nx1 matrix that defines the spin direction.
+
+if size(rf, 2) > 1
+    error('rf must be a nx1 matrix');
+end
+
 wf_nx6 = tm_1x3_to_1x6(1, 0, gwf);
 rf_nx6 = repmat(rf, 1, 6);
 
