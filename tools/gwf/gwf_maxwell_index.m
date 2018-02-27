892f3465122e650f45e87a4b89826741bf12a427
Filip Szczepankiewicz <filip.szczepankiewicz@gmail.com>
2018-02-27 17:05:31 +0100

    Updated text for maxwell index

--- tools/gwf/gwf_maxwell_index.m
+++ tools/gwf/gwf_maxwell_index.m
@@ -1,9 +1,16 @@
 function m = gwf_maxwell_index(gwf, rf, dt)
 % function m = gwf_maxwell_index(gwf, rf, dt)
-
-% gwf must be a nx3 matrix that defines directions along the time
-% dimension.
-% rf must be a nx1 matrix that defines the spin direction.
+% m is the "Maxwell index", as defined in 
+% Szczepankiewicz and Nilsson, ISMRM 2018
+% "Maxwell-compensated waveform design for asymmetric diffusion encoding"
+% Download abstract at: https://goo.gl/vVGQq2
+%
+% Briefly, the Maxwell index can be use to guage the impact of concomitant
+% fields on the signal. To estimate the signal error, please use
+% Concomitant Field Analysis tools (CFA), by calling cfa_*.
+%
+% gwf must be a nx3 matrix that defines directions along the time dimension.
+% rf  must be a nx1 matrix that defines the spin direction.
 
 if size(rf, 2) > 1
     error('rf must be a nx1 matrix');
