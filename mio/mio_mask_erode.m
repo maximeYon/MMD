44807897959c4b2eef2d10e309399a906f417710
Markus Nilsson <nilzzon@gmail.com>
2018-11-11 17:58:16 -0500

    Various improvements from FSZ

--- mio/mio_mask_erode.m
+++ mio/mio_mask_erode.m
@@ -1,8 +1,11 @@
-function M = mio_mask_erode(I)
+function M = mio_mask_erode(I, n)
 % function M = mio_mask_erode(I)
 %
 % erode the mask by 1 voxel
 
-n = 3;
+if (nargin < 2)
+    n = 3;
+end
+
 M = convn((double(I) > 0), ones(n,n,n), 'same') >= n^3;
 
