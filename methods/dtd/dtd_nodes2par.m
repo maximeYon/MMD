1497413415551f5ef5a856946624e8ca12ccfb06
Daniel Topgaard <topgaard@gmail.com>
2019-02-19 23:07:22 +0100

    Update dtd_nodes2par.m

--- methods/dtd/dtd_nodes2par.m
+++ methods/dtd/dtd_nodes2par.m
@@ -1,13 +1,20 @@
 function [n,par,perp,theta,phi] = dtd_nodes2par(dtd_nodes)
 
 if ~isempty(dtd_nodes)
-    n = dtd_nodes(1);
-    m = numel(dtd_nodes(2:end))/n;
-    dtd_nodes_array = reshape(dtd_nodes(2:end),[m n]);
-    par = dtd_nodes_array(1,:)';
-    perp = dtd_nodes_array(2,:)';
-    theta = dtd_nodes_array(3,:)';
-    phi = dtd_nodes_array(4,:)';
+    n = dtd_nodes(1);    
+    if n > 0
+        m = numel(dtd_nodes(2:end))/n;
+        dtd_nodes_array = reshape(dtd_nodes(2:end),[m n]);
+        par = dtd_nodes_array(1,:)';
+        perp = dtd_nodes_array(2,:)';
+        theta = dtd_nodes_array(3,:)';
+        phi = dtd_nodes_array(4,:)';
+    else
+        par = [];
+        perp = [];
+        theta = [];
+        phi = [];        
+    end
 else
     n = 0;
     par = [];
