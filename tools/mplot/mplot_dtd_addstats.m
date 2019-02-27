2984421e756af7de98246fc341ac69234e9ba582
Daniel Topgaard <daniel.topgaard@fkem1.lu.se>
2019-02-27 13:27:55 +0100

    Update dtd terminology

--- tools/mplot/mplot_dtd_addstats.m
+++ tools/mplot/mplot_dtd_addstats.m
@@ -6,7 +6,10 @@ hold(axh,'on')
 x = log10(dps.mdiso);
 y = log10((1+2*sqrt(dps.msdanison))/(1-sqrt(dps.msdanison)));
 if ~isreal(y)
-    y = log10(opt.dtd.dmax/opt.dtd.dmin);
+    y = abs(y);
+    if y > log10(opt.dtd.dmax/opt.dtd.dmin);
+        y = log10(opt.dtd.dmax/opt.dtd.dmin);
+    end
 end
 if isfield(dps,'signaniso')
     if dps.signaniso < 0
@@ -18,5 +21,10 @@ if isfield(dps,'signaniso')
 end
 x_low = log10(dps.mdiso-sqrt(dps.vdiso));
 x_high = log10(dps.mdiso+sqrt(dps.vdiso));
+if ~isreal(x_low)
+    x_low = abs(x_low);
+    x_high = abs(x_high);
+end
+
 ph = plot(axh,x,y,'ko',[x_low x_high],y*[1 1],'k-');
 set(ph,'Color',.7*[1 1 1],'LineWidth',2,'MarkerSize',6,'MarkerFaceColor',.7*[1 1 1])
