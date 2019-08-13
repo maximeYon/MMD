c00f5fd460929618395f7ff147ab53a9004f2c12
Daniel Topgaard <topgaard@gmail.com>
2019-08-13 17:46:21 +0200

    Add mdm_dps_collectbs

---
+++ mdm/mdm_dps_collectbs.m
@@ -0,0 +1,24 @@
+function bs_dps = mdm_dps_collectbs(method, bs_path, opt)
+% function bs_dps = mdm_dps_collectbs(method, bs_path, opt)
+%
+
+bsno = msf_getdirno(bs_path);        
+bs_dps = cell(numel(bsno),1);
+parfor nbs = 1:numel(bsno)
+    mfs_fn   = fullfile(bs_path,num2str(bsno(nbs)),'mfs.mat');
+    if exist(mfs_fn,'file')==2
+
+        mfs = mdm_mfs_load(mfs_fn);
+        m = mfs.m;
+
+        if strcmp(method,'dtr2d')
+            bs_dps{nbs} = dtr2d_4d_fit2param(m, [], opt);
+        elseif strcmp(method,'dtd')
+            bs_dps{nbs} = dtd_4d_fit2param(m, [], opt);
+        end
+        
+        bs_dps{nbs}.nii_h = mfs.nii_h;
+                
+    end
+end
+
---
+++ mdm/mdm_dps_median.m
@@ -0,0 +1,40 @@
+function median_dps = mdm_dps_median(bs_dps)
+% function median_dps = mdm_dps_median(bs_dps)
+%
+
+median_dps.nii_h = bs_dps{1}.nii_h;
+
+sz = ones(1,3);
+sz_temp = size(bs_dps{1}.s0);
+sz(1:numel(sz_temp)) = sz_temp;
+
+f = fieldnames(bs_dps{1});
+for c = 1:numel(f)
+    if (isstruct(bs_dps{1}.(f{c})))
+        continue;
+    elseif iscell(bs_dps{1}.(f{c}))
+
+        for cbin = 1:numel(bs_dps{1}.(f{c}))
+            fbin = fieldnames(bs_dps{1}.(f{c}){cbin});
+            for cfbin = 1:numel(fbin)
+                if (size(bs_dps{1}.(f{c}){cbin}.(fbin{cfbin}), 1) == sz(1) && numel(bs_dps{1}.(f{c}){cbin}.(fbin{cfbin}))<4) 
+                    ind_bs = find(~cellfun('isempty',bs_dps));
+                    ptemp = zeros(sz(1),sz(2),sz(3),numel(ind_bs));
+                    for nbs = 1:numel(ind_bs)
+                        ptemp(:,:,:,nbs) = bs_dps{ind_bs(nbs)}.(f{c}){cbin}.(fbin{cfbin});
+                    end
+                    median_dps.(f{c}){cbin}.(fbin{cfbin}) = median(ptemp,4);
+                end
+            end                   
+        end
+
+    elseif (size(bs_dps{1}.(f{c}), 1) == sz(1) && numel(bs_dps{1}.(f{c}))<4) 
+        ind_bs = find(~cellfun('isempty',bs_dps));
+        ptemp = zeros(sz(1),sz(2),sz(3),numel(ind_bs));
+        for nbs = 1:numel(ind_bs)
+            ptemp(:,:,:,nbs) = bs_dps{ind_bs(nbs)}.(f{c});
+        end
+        median_dps.(f{c}) = median(ptemp,4);
+    end
+end
+
