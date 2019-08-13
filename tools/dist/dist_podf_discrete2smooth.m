80f26dbbb7edf89ba812e1287777768841177520
Daniel Topgaard <topgaard@gmail.com>
2019-08-13 21:21:22 +0200

    Update dist

--- tools/dist/dist_podf_discrete2smooth.m
+++ tools/dist/dist_podf_discrete2smooth.m
@@ -28,12 +28,12 @@ clear K
 
 odf_s.w = k*odf_d.w;
 odf_s.diso = (k*(odf_d.w.*odf_d.diso))./odf_s.w;
-odf_s.sqddelta = (k*(odf_d.w.*odf_d.sqddelta))./odf_s.w;
+odf_s.sddelta = (k*(odf_d.w.*odf_d.sddelta))./odf_s.w;
 odf_s.r2 = (k*(odf_d.w.*odf_d.r2))./odf_s.w;
 odf_s.verts = repmat(odf_s.w,[1 3]).*[odf_s.x odf_s.y odf_s.z];
 odf_s.c = abs([odf_s.x odf_s.y odf_s.z]);
 
 odf_s.w(~isfinite(odf_s.w)) = 0;
 odf_s.diso(~isfinite(odf_s.diso)) = 0;
-odf_s.sqddelta(~isfinite(odf_s.sqddelta)) = 0;
+odf_s.sddelta(~isfinite(odf_s.sddelta)) = 0;
 odf_s.r2(~isfinite(odf_s.r2)) = 0;
