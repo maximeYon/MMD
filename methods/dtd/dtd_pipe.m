f516f9661f6350c9b1d70f19257aee34e489310c
Daniel Topgaard <topgaard@gmail.com>
2019-08-13 21:15:09 +0200

    Fix bsmerge

--- methods/dtd/dtd_pipe.m
+++ methods/dtd/dtd_pipe.m
@@ -35,11 +35,15 @@ if (opt.do_data2fit)
         msf_mkdir(fileparts(paths.ind_fn));
         ind = (opt.dtd.ind_start-0) + round(rand([s.xps.n-(opt.dtd.ind_start-1),1])*(s.xps.n-(opt.dtd.ind_start-0)));
         save(paths.ind_fn, 'ind');
-        ind_fn = mdm_ind_save(ind, paths.ind_fn);
-        load(paths.ind_fn)
+%         ind_fn = mdm_ind_save(ind, paths.ind_fn);
+%         load(paths.ind_fn)
         opt.bootstrap.ind = ind;
     end
     mdm_data2fit(@dtd_4d_data2fit, s, paths.mfs_fn, opt);
+    %Convert mfs.m to single to save disk space
+    mfs = mdm_mfs_load(paths.mfs_fn);
+    mfs.m = single(mfs.m);
+    save(paths.mfs_fn, 'mfs');
 end
 
 if (opt.do_datafit2chisq)
