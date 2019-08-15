55600e912de51cfe539ea8baa844a386c822324b
Daniel Topgaard <topgaard@gmail.com>
2019-08-15 14:59:00 +0200

    Update dtd_pipe.m
    Convert mfs.m to single to save disk space

--- methods/dtd/dtd_pipe.m
+++ methods/dtd/dtd_pipe.m
@@ -14,7 +14,7 @@ if (nargin < 3), opt.present = 1; end
 opt = mdm_opt(opt);
 opt = dtd_opt(opt);
 paths = mdm_paths(paths);
-msf_log(['Starting ' mfilename], opt);
+msf_log(['Starting ' mfilename ' ' paths.mfs_fn], opt);
 
 % Check that the xps is proper
 dtd_check_xps(s.xps, opt);
@@ -35,15 +35,15 @@ if (opt.do_data2fit)
         msf_mkdir(fileparts(paths.ind_fn));
         ind = (opt.dtd.ind_start-0) + round(rand([s.xps.n-(opt.dtd.ind_start-1),1])*(s.xps.n-(opt.dtd.ind_start-0)));
         save(paths.ind_fn, 'ind');
-        ind_fn = mdm_ind_save(ind, paths.ind_fn);
-        load(paths.ind_fn)
+%         ind_fn = mdm_ind_save(ind, paths.ind_fn);
+%         load(paths.ind_fn)
         opt.bootstrap.ind = ind;
     end
     mdm_data2fit(@dtd_4d_data2fit, s, paths.mfs_fn, opt);
-    %Convert mfs.m to single to save disk space
-    mfs = mdm_mfs_load(paths.mfs_fn);
-    mfs.m = single(mfs.m);
-    save(paths.mfs_fn, 'mfs');
+%     %Convert mfs.m to single to save disk space
+%     mfs = mdm_mfs_load(paths.mfs_fn);
+%     mfs.m = single(mfs.m);
+%     save(paths.mfs_fn, 'mfs');
 end
 
 if (opt.do_datafit2chisq)
