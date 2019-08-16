e589620f9c4874da3a0af2453626206122262ea6
Daniel Topgaard <topgaard@gmail.com>
2019-08-16 22:43:46 +0200

    Bugfix bootstrap dtr2d

--- methods/dtr2d/dtr2d_pipe.m
+++ methods/dtr2d/dtr2d_pipe.m
@@ -14,7 +14,7 @@ if (nargin < 3), opt.present = 1; end
 opt = mdm_opt(opt);
 opt = dtr2d_opt(opt);
 paths = mdm_paths(paths);
-msf_log(['Starting ' mfilename], opt);
+msf_log(['Starting ' mfilename ' ' paths.mfs_fn], opt);
 
 % Check that the xps is proper
 dtr2d_check_xps(s.xps, opt);
@@ -35,8 +35,8 @@ if (opt.do_data2fit)
         msf_mkdir(fileparts(paths.ind_fn));
         ind = (opt.dtr2d.ind_start-0) + round(rand([s.xps.n-(opt.dtr2d.ind_start-1),1])*(s.xps.n-(opt.dtr2d.ind_start-0)));
         save(paths.ind_fn, 'ind');
-%         ind_fn = mdm_ind_save(ind, paths.ind_fn);
-%         load(paths.ind_fn)
+        ind_fn = mdm_ind_save(ind, paths.ind_fn);
+        load(paths.ind_fn)
         opt.bootstrap.ind = ind;
     end
     mdm_data2fit(@dtr2d_4d_data2fit, s, paths.mfs_fn, opt);
