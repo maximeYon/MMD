43314a3760a691db6875f7b12a2f512e3eaf16d0
Daniel Topgaard <topgaard@gmail.com>
2020-01-22 18:39:53 +0100

    Update mplot_globalstats.m

--- tools/mplot/mplot_globalstats.m
+++ tools/mplot/mplot_globalstats.m
@@ -48,6 +48,57 @@ elseif strcmp(method,'dtr1d')
     set(gca,'XLim',xlim)
 end
 
+if strcmp(method,'dtd')
+    opt = mplot_opt(opt);
+
+    left = left+dleft;
+    dist_d.w = dps.s0(:);
+    dist_d.x = dps.mdiso(:);
+    dist_d.y = dps.msddelta(:);
+    dist_d.n = numel(dist_d.w);
+
+    axpars.xmin = clim.mdiso(1);
+    axpars.xmax = clim.mdiso(2);
+    axpars.ymin = clim.msddelta(1);
+    axpars.ymax = clim.msddelta(2);
+    contourpars.Nx = 50;
+    contourpars.Ny = contourpars.Nx;
+    contourpars.Nlevels = 20;
+
+    dist_s.x = linspace(axpars.xmin,axpars.xmax,contourpars.Nx)';
+    dist_s.y = linspace(axpars.ymin,axpars.ymax,contourpars.Ny)';
+    dist_s.xsigma = 2*abs(dist_s.x(2) - dist_s.x(1));
+    dist_s.ysigma = 2*abs(dist_s.y(2) - dist_s.y(1));
+
+    dist_s = dist_2d_discrete2smooth(dist_d,dist_s);
+
+    C = contourc(double(dist_s.x),double(dist_s.y),double(dist_s.w'),contourpars.Nlevels);
+
+    axh = axes('position',[left bottom width height]);
+    hold(axh,'on')
+
+    hcontour = [];
+    count = 1;
+    while count < length(C)
+        numxy = C(2,count);
+        xtemp = C(1,count+(1:numxy));
+        ytemp = C(2,count+(1:numxy));
+        h = plot(axh,xtemp,ytemp,'k-','LineWidth',.5*opt.mplot.lw);
+        hcontour = [hcontour; h];
+        count = count + numxy + 1;
+    end
+
+    xlabel('size, E[D_{iso}] / m^2s^{-1}')
+    xlim = clim.mdiso + .05*abs(diff(clim.mdiso))*[-1 1];
+    ylabel('shape, E[D_\Delta^2]')
+    ylim = clim.msddelta + .05*abs(diff(clim.msddelta))*[-1 1];
+    set(gca,'XLim',xlim)
+    set(gca,'YLim',ylim)
+    axis square
+    set(axh,'Box','off','TickDir','out','TickLength',.02*[1 1],'FontSize',opt.mplot.fs,'LineWidth',opt.mplot.lw)
+
+end
+
 papersize = 17.5*[1 1/3];
 set(gcf, 'PaperUnits','centimeters','PaperPosition', [0 0 papersize],'PaperSize', papersize); 
 
